8d9ffd34540675f0d3918115bdd9e42d47551756
meheraz1100 <meheraz1100@gmail.com>
2024-07-10 11:02:01 +0600

    fourth line added

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -4,6 +4,7 @@ const SignUp = () => {
             <h1>Hello World</h1>
             <h1>Second Line Added</h1>
             <h1>Third Line added</h1>
+            <h1>Fourth line</h1>
         </div>
     );
 };
