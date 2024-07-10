3d06beec2b96e687cf35b854789ab395ab24a2ae
shakurShirajul <siirajulisllam@gmail.com>
2024-07-10 11:01:42 +0600

    Unwanted Text Removed

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,6 @@
 const SignUp = () => {
     return (
         <div>
-            <h1>Hello World</h1>
-            <h1>Second Line Added</h1>
-            <h1>Third Line added</h1>
         </div>
     );
 };
